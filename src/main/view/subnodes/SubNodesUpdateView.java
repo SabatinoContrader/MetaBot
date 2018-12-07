0367417ba8f57163d6b6e9a8480e5b78923730c9
Fabio <f.giancaspro@contrader.it>
2018-12-07 12:24:08 +0100

    subNodes model sub nodes DAO

--- src/main/view/subnodes/SubNodesUpdateView.java
+++ src/main/view/subnodes/SubNodesUpdateView.java
@@ -9,12 +9,12 @@ import main.controller.SubNodesController;
 import java.util.List;
 import java.util.Scanner;
 
-public class SubNodesDeleteView implements View {
+public class SubNodesUpdateView implements View {
 
 	private SubNodesController subNodesController;
 	private Request request;
-
-	public SubNodesDeleteView() {
+	
+	public SubNodesUpdateView() {
 		this.subNodesController = new SubNodesController();
 	}
 
@@ -24,15 +24,12 @@ public class SubNodesDeleteView implements View {
 
 	@Override
 	public void showOptions() {
-		List<SubNodes> subNodes;
-		Integer idSubNodes;
-        subNodes = subNodesController.getAllSubNodes();
-		System.out.println("----- Scegli Id per cancellare -----\n");
+		List<SubNodes> subNodes = subNodesController.getAllSubNodes();
+		System.out.println("----- Gli sub_nodes nel tuo database sono -----");
+		System.out.println();
 		subNodes.forEach(us_type -> System.out.println(us_type.toString()));
-		System.out.println("id_sub_nodes: \n");
-		idSubNodes = Integer.parseInt(getInput());
-		if (idSubNodes != null)
-			subNodesController.deleteSubNodes(new SubNodes(idSubNodes, 0, 0, 0));
+		System.out.println();
+		
 	}
 
 	@Override
@@ -46,7 +43,7 @@ public class SubNodesDeleteView implements View {
 		request = new Request();
 		request.put("mode", "menu");
 		request.put("choice", 0);
-		MainDispatcher.getInstance().callAction("SubNodes", "doControl", request);
+	    MainDispatcher.getInstance().callAction("SubNodes", "doControl", request);
 	}
 
 }
