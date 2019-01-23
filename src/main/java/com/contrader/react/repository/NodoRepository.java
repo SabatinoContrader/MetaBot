708a0c3cbafdf68239d5f2a7a95552214ea01c77
radek-micek <45622327+radek-micek@users.noreply.github.com>
2019-01-23 11:23:07 +0100

    giacomo pulizia codice

--- src/main/java/com/contrader/react/repository/NodoRepository.java
+++ src/main/java/com/contrader/react/repository/NodoRepository.java
@@ -20,9 +20,6 @@ public interface NodoRepository extends CrudRepository<Nodo, Integer> {
 	@Query(value = "select * from nodo n where n.id_nodo_padre is null and n.id_nodo not in (select c.id_nodo from chatbot c ); ", nativeQuery = true)
 	public List<Nodo> nodiSenzaPadreDisponibili();
 
-	@Query(value = "select 1 from nodo n where n.id_nodo_padre = ?1", nativeQuery = true)
-	public Integer findUserByIdNodoPadre(Integer id);
-
 	public List<Nodo> findAllByNodoPadre(Nodo nodoPadre);
 
 }
